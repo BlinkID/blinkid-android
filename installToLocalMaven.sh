e458218c12f02b2f88ebccfb6876cc584b838863
Nenad Mikša <nenad.miksa@microblink.com>
2016-03-18 15:09:15 +0100

    prepare gradle files for next release

--- installToLocalMaven.sh
+++ installToLocalMaven.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
 
-version=`grep '##' "Release notes.md" | head -n 1 | tr -d '#' | tr -d ' '`
-
-mvn install:install-file -Dfile=LibRecognizer.aar -DgroupId=com.microblink -DartifactId=blinkid -Dversion=$version -Dpackaging=aar -DcreateChecksum=true -Djavadoc=./LibRecognizer-javadoc.jar
+mvn install:install-file -Dfile=LibRecognizer.aar -DpomFile=pom.xml -DcreateChecksum=true -Djavadoc=LibRecognizer-javadoc.jar
