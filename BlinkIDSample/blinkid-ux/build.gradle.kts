ca105718f66559aaeb54a4f1ed0f566f40841cbc
sandra_zivkovic <sandra.zivkovic@microblink.com>
2025-10-29 16:22:46 +0100

    [v7.6.0] target/compile sdk 36

--- BlinkIDSample/blinkid-ux/build.gradle.kts
+++ BlinkIDSample/blinkid-ux/build.gradle.kts
@@ -7,7 +7,7 @@ plugins {
 
 android {
     namespace = "com.microblink.blinkid.ux"
-    compileSdk = 35
+    compileSdk = 36
 
     defaultConfig {
         minSdk = 24
--- BlinkIDSample/lib-common/build.gradle.kts
+++ BlinkIDSample/lib-common/build.gradle.kts
@@ -7,7 +7,7 @@ plugins {
 
 android {
     namespace = "com.microblink.blinkid.sample"
-    compileSdk = 35
+    compileSdk = 36
 
     defaultConfig {
         minSdk = 24
